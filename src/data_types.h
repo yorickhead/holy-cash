9bd3a343b31522cf628ba78a65889c16d9a3564f
osami <v.pylayev@mail.ru>
2026-04-09 06:52:57 +0500

    update data_types

--- src/data_types.h
+++ src/data_types.h
@@ -1,27 +1,27 @@
 typedef enum {
-    STRING,
-    HASH,
-    LIST,
-}DataType;
+  STRING,
+  HASH,
+  LIST,
+} DataType;
 
 typedef struct {
-    DataType type;
-    void* value;
-}DataObject;
+  DataType type;
+  void *value;
+} DataObject;
 
 typedef struct {
-    int size;
-    DataObject* elements;
-}List;
+  int size;
+  DataObject *elements;
+} List;
 
 typedef struct {
-    char* key;
-    DataObject value;
-}KeyValuePair;
+  char *key;
+  DataObject value;
+} KeyValuePair;
 
 typedef struct {
-    int size;
-    KeyValuePair* pairs;
-}Hash;
+  int size;
+  KeyValuePair *pairs;
+} Hash;
 
-typedef char* String;
+typedef char *String;
