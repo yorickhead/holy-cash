dac745be7b0986097b8f2df83a1eddfbd33d0da2
osami <v.pylayev@mail.ru>
2026-04-16 06:36:18 +0500

    update parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "data_types.h"
 #include "storage.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,159 +12,112 @@ typedef enum {
   GET,
   HASH_REMOVE,
   REMOVE,
+  UNKNOWN,
 } RequestType;
 
-RequestType *request_type_from_req(char *req) {
-  RequestType *rt = malloc(sizeof(RequestType));
-  if (!rt) {
-    perror("failed allocate memory for request type");
-
-    return NULL;
-  }
+RequestType request_type_from_req(char *req) {
+  RequestType rt;
 
   if (strcmp(req, "get") == 0) {
-    *rt = GET;
+    rt = GET;
   } else if (strcmp(req, "set") == 0) {
-    *rt = SET;
+    rt = SET;
   } else if (strcmp(req, "remove") == 0) {
-    *rt = REMOVE;
+    rt = REMOVE;
+  } else if (strcmp(req, "hset") == 0) {
+    rt = HASH_SET;
+  } else if (strcmp(req, "hget") == 0) {
+    rt = HASH_GET;
+  } else if (strcmp(req, "hremove") == 0) {
+    rt = HASH_REMOVE;
   } else {
     printf("ERROR unknown request type: %s", req);
-    return NULL;
+
+    rt = UNKNOWN;
   }
 
   return rt;
 }
 
 Result *route_remove_request(Storage *storage, char *key) {
-  Result *res = malloc(sizeof(Result));
-  if (!res) {
-    perror("failed allocate memory for request");
-
-    return NULL;
-  }
-
-  res->message = calloc(32, sizeof(char));
-  if (!res->message) {
-    perror("failed allocate memory for message");
-
-    free(res);
-
-    return NULL;
-  }
-
   int status = storage_remove(storage, key);
   if (status != 0) {
-    strcpy(res->message, "failed remove from storage");
+    ERROR_RESULT("failed remove from storage");
   } else {
-    strcpy(res->message, "success");
+    SUCCESS_RESULT(NULL)
   }
-
-  return res;
 }
 
 Result *route_get_request(Storage *storage, char *key) {
-  Result *res = malloc(sizeof(Result));
-  if (!res) {
-    perror("failed allocate memory for request");
-
-    return NULL;
-  }
-
-  res->value = NULL;
-
-  res->message = calloc(32, sizeof(char));
-
   DataObject *obj = storage_get(storage, key);
   if (obj == NULL) {
-    strcpy(res->message, "failed find value in cash");
+    ERROR_RESULT("failed fetch object from storage");
   } else {
-    res->value = obj;
-
-    strcpy(res->message, "success");
+    SUCCESS_RESULT(obj);
   }
-
-  return res;
 }
 
 Result *route_set_request(Storage *storage, char *key, void *value) {
-  Result *res = malloc(sizeof(Result));
-  if (!res) {
-    perror("failed allocate memory for request");
-
-    return NULL;
-  }
-
-  res->message = calloc(32, sizeof(char));
-  if (!res->message) {
-    perror("failed allocate memory for res message");
-    free(res);
-
-    return NULL;
-  }
-
   DataObject *obj = malloc(sizeof(DataObject));
+  if (!obj) {
+    ERROR_RESULT("failed create DataObject");
+  }
+
   obj->type = STRING;
   obj->value = value;
 
   int status = storage_set(storage, key, obj);
   if (status != 0) {
-    perror("failed set value");
-
-    return NULL;
+    ERROR_RESULT("failed set object in storage");
   }
 
-  return res;
+  SUCCESS_RESULT(obj);
 }
 
 Result *route_hset_request(Storage *storage, char *storage_key, char *h_key,
-                           void *value) {
-
-  Result *res = malloc(sizeof(Result));
-  if (!res) {
-    perror("failed allocate memory for request");
-
-    return NULL;
-  }
-
-  res->message = calloc(32, sizeof(char));
-  if (!res->message) {
-    perror("failed allocate memory for res message");
-    free(res);
-
-    return NULL;
-  }
-
+                           String value) {
   DataObject *hash_table = storage_get(storage, storage_key);
+
+  Hash *hash;
+
   if (hash_table == NULL) {
-    DataObject *obj = malloc(sizeof(DataObject));
-    if (!obj) {
-      perror("failed allocate memory for data object");
-
-      free(res->message);
-      free(res);
-
-      return NULL;
+    hash = create_hash_table();
+    if (hash == NULL) {
+      ERROR_RESULT("failed create hash table");
+    }
+  } else {
+    if (hash_table->type != HASH) {
+      ERROR_RESULT("hash table object type is not HASH");
     }
 
-    obj->type = HASH;
-
-    Hash *table = malloc(sizeof(Hash));
+    hash = (Hash *)hash_table->value;
   }
+
+  int status = hash_set(hash, h_key, value);
+  if (status != 0) {
+    ERROR_RESULT("failed set value in hash");
+  }
+
+  hash_table->value = (void *)hash;
+
+  status = storage_set(storage, storage_key, hash_table);
+  if (status != 0) {
+    ERROR_RESULT("failed set obj in storage");
+  }
+
+  SUCCESS_RESULT(NULL)
 }
 
-Result *route_request(char *request) {
+Result *route_request(Storage *storage, char *request) {
   char *token;
   char *saveptr;
 
   token = strtok_r(request, " ", &saveptr);
   char *func = strdup(token);
 
-  RequestType *rt = request_type_from_req(request);
-  if (rt == NULL) {
-    perror("failed parse request type");
-
-    return NULL;
+  RequestType rt = request_type_from_req(request);
+  if (rt == UNKNOWN) {
+    ERROR_RESULT("unknown request type");
   }
 
   token = strtok_r(NULL, " ", &saveptr);
@@ -180,5 +134,13 @@ Result *route_request(char *request) {
     args = strdup(saveptr);
   } else {
     return NULL;
+  }
+
+  switch (rt) {
+  case REMOVE:
+    route_remove_request(storage, key);
+  case GET:
+    route_get_request(storage, key);
+  case SET:
   }
 }
