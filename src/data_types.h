a4e59077e624bf13eec404c909ce83af880ceed3
osami <v.pylayev@mail.ru>
2026-04-16 06:36:08 +0500

    update data_types.h

--- src/data_types.h
+++ src/data_types.h
@@ -3,28 +3,33 @@
 #define DATA_TYPES_H
 
 typedef enum {
-    STRING,
-    HASH,
-    LIST,
+  STRING,
+  HASH,
+  LIST,
 } DataType;
 
 typedef struct {
-    DataType type;
-    void *value;
+  DataType type;
+  void *value;
 } DataObject;
 
 typedef char *String;
 
 typedef struct {
-    char *key;
-    String value;
+  char *key;
+  String value;
 } KeyValuePair;
 
 typedef struct {
-    int size;           
-    int count;          
-    KeyValuePair *pairs;
+  int size;
+  int count;
+  KeyValuePair *pairs;
 } Hash;
+
+Hash *create_hash_table(void);
+
+int hash_set(Hash *table, char *key, String value);
+String hash_get(Hash *table, const char *key);
 
 void destroy_hash(Hash *hash);
 int hash_set(Hash *hash, char *key, String value);
