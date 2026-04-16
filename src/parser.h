5d7680af90a92bfffcd2017e4e4a11829a11010c
osami <v.pylayev@mail.ru>
2026-04-16 06:36:28 +0500

    update parser.h

--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,28 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#define ERROR_RESULT(msg)                                                      \
+  do {                                                                         \
+    Result *res = malloc(sizeof(Result));                                      \
+    if (!res)                                                                  \
+      return NULL;                                                             \
+    res->value = NULL;                                                         \
+    if (asprintf(&res->message, "ERROR: %s", (msg)) == -1) {                   \
+      free(res);                                                               \
+      return NULL;                                                             \
+    }                                                                          \
+    return res;                                                                \
+  } while (0)
+
+#define SUCCESS_RESULT(obj)                                                    \
+  {                                                                            \
+    Result *res = malloc(sizeof(Result));                                      \
+    res->message = calloc(8, sizeof(char));                                    \
+    res->value = obj;                                                          \
+    strcpy(res->message, "success");                                           \
+    return res;                                                                \
+  }
+
 #include "storage.h"
 #include <stdbool.h>
 
